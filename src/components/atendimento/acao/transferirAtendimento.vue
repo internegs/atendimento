dda6fd1425932a01b9697d44f6f89b45eb10a7b6
Moises-Drum <moises.drumand18@gmail.com>
2025-11-18 14:14:59 -0400

    :sparkles: feat: adicionado carregamento em transferirAtendimento.vue

--- src/components/atendimento/acao/transferirAtendimento.vue
+++ src/components/atendimento/acao/transferirAtendimento.vue
@@ -1,163 +1,195 @@
 <template>
-  <Container>
-    <!-- Modal -->
-    <div
-      class="modal fade"
-      id="transfereAtendimento"
-      tabindex="-1"
-      aria-labelledby="transfereAtendimento"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">
-              Tansferir Atendimento
-            </h1>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <form action="" class="row">
-              <div class="form-group col-md-12">
-                <select
-                  name=""
-                  id=""
-                  class="form-select"
-                  v-model="atendenteSelecionado"
-                >
-                  <option value="" selected disabled>
-                    Selecione um atendente
-                  </option>
-                  <option
-                    :value="atendente.id"
-                    v-for="(atendente, index) in atendentesDisponiveis"
-                    :key="index"
-                  >
-                    {{ atendente.name }} - <span class="badge bg-primary rounded-pill" v-if="atendente.online == 0 "  > Off</span>
-                    <span class="badge bg-warning rounded-pill" v-if="atendente.online == 1 " > On</span>
-                  </option>
-                </select>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-primary"
-              @click="transfereAtendimento"
-            >
-              Transferir
-            </button>
-          </div>
+    <container>
+        <!-- Modal -->
+        <div
+            id="transfereAtendimento"
+            class="modal fade"
+            tabindex="-1"
+            aria-labelledby="transfereAtendimento"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1
+                            id="exampleModalLabel"
+                            class="modal-title fs-5"
+                        >
+                            Tansferir Atendimento
+                        </h1>
+                        <button
+                            type="button"
+                            class="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                        ></button>
+                    </div>
+
+                    <div class="modal-body">
+                        <form
+                            action=""
+                            class="row"
+                        >
+                            <div class="form-group col-md-12">
+                                <select
+                                    v-model="atendenteSelecionado"
+                                    class="form-select"
+                                >
+                                    <option
+                                        value=""
+                                        selected
+                                        disabled
+                                    >
+                                        Selecione um atendente
+                                    </option>
+
+                                    <option
+                                        v-for="(atendente, index) in atendentesDisponiveis"
+                                        :key="index"
+                                        :value="atendente.id"
+                                    >
+                                        {{ atendente.name }} -
+                                        <span
+                                            v-if="atendente.online == 0"
+                                            class="badge bg-primary rounded-pill"
+                                        >
+                                            Off
+                                        </span>
+                                        <span
+                                            v-if="atendente.online == 1"
+                                            class="badge bg-warning rounded-pill"
+                                        >
+                                            On
+                                        </span>
+                                    </option>
+                                </select>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button
+                            type="button"
+                            class="btn btn-primary"
+                            @click="transfereAtendimento"
+                        >
+                            <span v-if="!btnLoading">Transferir</span>
+
+                            <div
+                                v-else
+                                class="spinner-border spinner-border-sm"
+                                role="status"
+                            >
+                                <span class="visually-hidden">Loading...</span>
+                            </div>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </Container>
+    </container>
 </template>
-  
-  <script>
-import Container from "@/components/GLOBALS/container.vue";
-import Api from "@/services/api";
-import Swal from 'sweetalert2';
-// import Swal from "sweetalert2";
+
+<script>
+import Container from '@/components/GLOBALS/container.vue'
+import Api from '@/services/api'
+import Swal from 'sweetalert2'
 
 export default {
-  name: "transfereAtendimento",
-  components: { Container },
-  data() {
-    return {
-      atendentesDisponiveis: [],
-      atendenteSelecionado: "",
-    };
-  },
-  props: {
-    id_atendimento: {
-      type: Number,
+    name: 'transfereAtendimento',
+
+    components: { Container },
+
+    props: {
+        id_atendimento: {
+            type: Number,
+        },
+        chamarDados: {
+            type: Function,
+        },
+        atualizaMeusAtendimentos: {
+            type: Function,
+        },
+        fecharTelaDeConversa: {
+            type: Function,
+        },
     },
-    chamarDados: {
-      type: Function
+
+    data() {
+        return {
+            atendentesDisponiveis: [],
+            atendenteSelecionado: '',
+            btnLoading: false,
+        }
     },
-    atualizaMeusAtendimentos: {
-      type: Function
+
+    mounted() {
+        this.chamarAtendentesDisponiveis()
     },
-    fecharTelaDeConversa: {
-      type: Function
-    }
-  },
-  methods: {
-    chamarAtendentesDisponiveis() {
-      Api.post("/atendentes_online/ZmlsYWRlYXRlbmRpbWVudG8=", {
-        id: localStorage.getItem("@USER_ID"),
-      })
-        .then((response) => {
-          this.atendentesDisponiveis = response.data.atendentes;
-         
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+
+    methods: {
+        chamarAtendentesDisponiveis() {
+            Api.post('/atendentes_online/ZmlsYWRlYXRlbmRpbWVudG8=', {
+                id: localStorage.getItem('@USER_ID'),
+            })
+                .then(response => {
+                    this.atendentesDisponiveis = response.data.atendentes
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+        },
+
+        transfereAtendimento() {
+            try {
+                const objTransfere = {
+                    id: localStorage.getItem('@USER_ID'),
+                    atendente_id_transfere: this.atendenteSelecionado,
+                    id_atendimento: this.id_atendimento,
+                }
+
+                const response = Api.post(
+                    '/transfere_atendimento/ZmlsYWRlYXRlbmRpbWVudG8=',
+                    objTransfere
+                )
+
+                this.fecharTelaDeConversa()
+
+                const truck_modal = document.querySelector('#transfereAtendimento')
+                const modal = bootstrap.Modal.getInstance(truck_modal)
+                modal.hide()
+
+                if (response.status === 200) {
+                    Swal.fire('Transferido!', response.data, 'success')
+                } else {
+                    throw new Error(response.data.mensagem)
+                }
+
+                this.chamarDados()
+
+                this.atualizaMeusAtendimentos()
+            } catch (error) {
+                this.btnLoading = false
+
+                console.error(error)
+
+                Swal.fire('Erro!', error.data.message || 'Erro ao transferir atendimento', 'error')
+            } finally {
+                this.btnLoading = false
+            }
+        },
     },
-    transfereAtendimento() {
-      const objTransfere = {
-        id: localStorage.getItem("@USER_ID"),
-        atendente_id_transfere: this.atendenteSelecionado,
-        id_atendimento: this.id_atendimento,
-      };
-
-      Api.post("/transfere_atendimento/ZmlsYWRlYXRlbmRpbWVudG8=", objTransfere)
-        .then((response) => {
-          
-          
-          this.fecharTelaDeConversa();
-
-          const truck_modal = document.querySelector("#transfereAtendimento");
-          const modal = bootstrap.Modal.getInstance(truck_modal);
-          modal.hide();
-
-          if (response.status == 200){
-
-
-          Swal.fire("Transferido!", response.data, "success");
-//          console.log(response.data)
-          } else {
-            Swal.fire("Erro!", response.data.mensagem, "error"); 
-            console.log(response.status)
-          }
-
-          this.chamarDados();
-
-          this.atualizaMeusAtendimentos();
-
-
-        })
-        .catch((error) => {
-          Swal.fire("Gravado!", error.data.mensagem, "error");
-        });
-    },
-    
-  },
-
-  mounted() {
-    this.chamarAtendentesDisponiveis();
-  },
-};
-</script>
-  
-  <style>
-.form-pad {
-  padding: 10px 20px !important;
 }
+</script>
 
+<style>
+.form-pad {
+    padding: 10px 20px !important;
+}
 
 @media (max-width: 600px) {
-  button {
-    width: 100%;
-  }
+    button {
+        width: 100%;
+    }
 }
 </style>
-  
